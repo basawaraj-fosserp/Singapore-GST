051b290497a29ad1c1035718fa79adec55ef4b09
Akash <akash@earthianslive.com>
2023-01-06 00:00:02 +0530

    fix: Process Statement of Account - SOA - div issue

--- singapore_l10n/public/js/process_statement_of_accounts.js
+++ singapore_l10n/public/js/process_statement_of_accounts.js
@@ -30,10 +30,10 @@ var set_html = function(frm, r) {
 				Company Registration No:  ${r.tax_id?r.tax_id:''}<br>
 				GST Registration No: ${r.tax_id?r.tax_id:''}
 			</div>
-			<div style="width: 20%; float: right;">
+			<div style="width: 10%; float: right;">
 			<br>
 		</div>
-			<div style="margin-left: 30%; float: right;"><br><br><br>
+			<div style="margin-left: 25%; float: left;"><br><br><br>
 				Tel : ${(r.cod_data && r.cod_data.phone)?r.cod_data.phone:''}<br>
 				Fax : ${(r.cod_data && r.cod_data.fax)?r.cod_data.fax:''}<br>
 				Email : ${(r.cod_data && r.cod_data.email_id)?r.cod_data.email_id:''}
